236050eab20ff68b29e56575e9f0d37edf9a2868
Juan Alvarez <juanandres140299@gmail.com>
2025-03-19 11:51:19 -0400

    Chapter 1: binary search

---
+++ 1-binary-search.ts
@@ -0,0 +1,31 @@
+// Exercise: create a function for binary search.
+
+/**
+ * Search an item in a sorted array.
+ * @constructor
+ * @param {T[]} input - The sorted array.
+ * @param {T} target - The item you are looking for.
+ */
+export default function binarySearch<T>(input: Array<T>, target: T) {
+  let low = 0;
+  let high = input.length - 1;
+
+  while (low <= high) {
+    let middle = Math.round((low + high) / 2);
+    let guess = input[middle];
+
+    if (guess === target) {
+      return middle;
+    } else if (guess > target) {
+      high = middle - 1;
+    } else {
+      low = middle + 1;
+    }
+  }
+
+  return null;
+}
+
+const list = [1, 3, 5, 7, 9];
+console.log(binarySearch(list, 3));
+console.log(binarySearch(list, -1));
