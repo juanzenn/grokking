949f83864e6ab3d36a6a448ce2806879a4da1076
Juan Alvarez <juanandres140299@gmail.com>
2025-03-19 21:06:29 -0400

    feat: add Vitest for testing cases

---
+++ src/tests/binary-search.spec.ts
@@ -0,0 +1,12 @@
+import { expect, test } from "vitest";
+import binarySearch from "../1-binary-search";
+
+const TEST_ARRAY = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+test("Finds the number 5 in the array", () => {
+  expect(binarySearch(TEST_ARRAY, 5)).toBe(4);
+});
+
+test("Returns null when item doesn't exist in the array", () => {
+  expect(binarySearch(TEST_ARRAY, 11)).toBe(null);
+});
---
+++ src/tests/selection-sort.spec.ts
@@ -0,0 +1,18 @@
+import { expect, test } from "vitest";
+import selectionSort from "../2-selection-sort";
+
+const NORMAL_INPUT = [1, 3, 5, 7, 2];
+const INPUT_WITH_EMPTY = [4, 1, 2, null];
+const EQUAL_LIST = [1, 1, 1, 1, 1];
+
+test("Orders the array correctly", () => {
+  expect(selectionSort(NORMAL_INPUT)).toStrictEqual([1, 2, 3, 5, 7]);
+});
+
+test("Returns null when item doesn't exist in the array", () => {
+  expect(selectionSort(INPUT_WITH_EMPTY)).toStrictEqual([null, 1, 2, 4]);
+});
+
+test("Returns the same array", () => {
+  expect(selectionSort(EQUAL_LIST)).toStrictEqual(EQUAL_LIST);
+});
