297d3f3ded367add977b28c2eabf5ae7c038fbad
Juan Alvarez <juanandres140299@gmail.com>
2025-03-19 21:21:23 -0400

    chore: create new test with NaN for sorting

--- tests/selection-sort.spec.ts
+++ tests/selection-sort.spec.ts
@@ -3,14 +3,19 @@ import selectionSort from "../src/2-selection-sort";
 
 const NORMAL_INPUT = [1, 3, 5, 7, 2];
 const INPUT_WITH_EMPTY = [4, 1, 2, null];
+const INPUT_WITH_NAN = [4, 1, 2, NaN];
 const EQUAL_LIST = [1, 1, 1, 1, 1];
 
 test("Orders the array correctly", () => {
   expect(selectionSort(NORMAL_INPUT)).toStrictEqual([1, 2, 3, 5, 7]);
 });
 
-test("Returns null when item doesn't exist in the array", () => {
+test("Orders the array with null first", () => {
   expect(selectionSort(INPUT_WITH_EMPTY)).toStrictEqual([null, 1, 2, 4]);
+});
+
+test("Orders the array with NaN first", () => {
+  expect(selectionSort(INPUT_WITH_NAN)).toStrictEqual([1, 2, 4, NaN]);
 });
 
 test("Returns the same array", () => {
